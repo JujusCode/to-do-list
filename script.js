ea79a94c3cf7697b78e901c126ed5c8c319c3b7d
JujusCode <jjbards99@gmail.com>
2025-02-25 13:59:07 +0800

    final process

--- script.js
+++ script.js
@@ -9,5 +9,32 @@ function addButton() {
     let li = document.createElement("li");
     li.innerHTML = taskInput.value;
     list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  taskInput.value = "";
+  saveData();
 }
+
+list.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", list.innerHTML);
+}
+function loadData() {
+  list.innerHTML = localStorage.getItem("data");
+}
+loadData();
